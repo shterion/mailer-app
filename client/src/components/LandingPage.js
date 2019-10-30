0a91d4c6e6875365b1fff93720e1fc5bb9c34433
Shterion Yanev <shterion.yanev@yahoo.com>
2019-10-30 08:23:16 +0100

    Fix setting state twice

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
 import DatePicker from './options/DatePicker';
 import Header from './common/Header';
 import SendOptions from './options/SendOptions';
@@ -12,128 +13,137 @@ import Ends from './options/Ends';
 import { sendMail } from '../actions/emailActions';
 
 class LandingPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipient: '',
-      text: '',
-      date: new Date(),
-      repeatType: 'once',
-      repeatAt: [],
-      neverEnd: false,
-      endOn: '',
-      endAfterOccurrences: 0,
-      sendType: 'now',
-      readyToSend: false,
-    };
-    this.baseState = this.state;
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleTypeChange = this.handleTypeChange.bind(this);
-    this.handleDaysClick = this.handleDaysClick.bind(this);
-    this.handleNeverEnd = this.handleNeverEnd.bind(this);
-    this.handleOccurrencesUp = this.handleOccurrencesUp.bind(this);
-    this.handleOccurrencesDown = this.handleOccurrencesDown.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			recipient: '',
+			text: '',
+			date: new Date(),
+			repeatType: 'once',
+			repeatAt: [],
+			neverEnd: false,
+			endOn: '',
+			endAfterOccurrences: 0,
+			sendType: 'now',
+			readyToSend: false,
+			sent: false
+		};
+		this.baseState = this.state;
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+		this.handleDateChange = this.handleDateChange.bind(this);
+		this.handleTypeChange = this.handleTypeChange.bind(this);
+		this.handleDaysClick = this.handleDaysClick.bind(this);
+		this.handleNeverEnd = this.handleNeverEnd.bind(this);
+		this.handleOccurrencesUp = this.handleOccurrencesUp.bind(this);
+		this.handleOccurrencesDown = this.handleOccurrencesDown.bind(this);
+	}
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-    if (e.target.value === 'custom' && this.state.repeatType === 'once') {
-      return this.setState({ readyToSend: false });
-    }
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+		if (e.target.value === 'custom' && this.state.repeatType === 'once') {
+			return this.setState({ readyToSend: false });
+		}
 
-    if (e.target.value === 'now' || e.target.value === 'later') {
-      return this.setState({ readyToSend: true });
-    }
-  }
+		if (e.target.value === 'now' || e.target.value === 'later') {
+			return this.setState({ readyToSend: true });
+		}
+		this.setState({ sent: false });
+	}
 
-  handleNeverEnd() {
-    this.setState({ neverEnd: !this.state.neverEnd, endOn: '', endAfterOccurrences: 0 });
-  }
+	handleNeverEnd() {
+		this.setState({ neverEnd: !this.state.neverEnd, endOn: '', endAfterOccurrences: 0 });
+	}
 
-  handleOccurrencesUp() {
-    this.setState({ endOn: '', neverEnd: false });
-    this.setState(prevState => ({ endAfterOccurrences: prevState.endAfterOccurrences + 1 }));
-  }
+	handleOccurrencesUp() {
+		this.setState({ endOn: '', neverEnd: false });
+		this.setState((prevState) => ({ endAfterOccurrences: prevState.endAfterOccurrences + 1 }));
+	}
 
-  handleOccurrencesDown() {
-    this.setState({ endOn: '', neverEnd: false });
-    this.setState(prevState => ({ endAfterOccurrences: prevState.endAfterOccurrences - 1 }));
-  }
+	handleOccurrencesDown() {
+		this.setState({ endOn: '', neverEnd: false });
+		this.setState((prevState) => ({ endAfterOccurrences: prevState.endAfterOccurrences - 1 }));
+	}
 
-  handleDateChange(date) {
-    if (date.endOn) {
-      return this.setState({ endOn: date.endOn, neverEnd: false, endAfterOccurrences: 0 });
-    }
-    this.setState({ date, neverEnd: false });
-  }
+	handleDateChange(date) {
+		if (date.endOn) {
+			return this.setState({ endOn: date.endOn, neverEnd: false, endAfterOccurrences: 0 });
+		}
+		this.setState({ date, neverEnd: false });
+	}
 
-  handleTypeChange(repeatType) {
-    if (repeatType !== 'weekly') {
-      this.setState({ repeatAt: [] });
-    }
-    this.setState({ repeatType });
-    this.setState({ readyToSend: true });
-  }
+	handleTypeChange(repeatType) {
+		if (repeatType !== 'weekly') {
+			this.setState({ repeatAt: [] });
+		}
 
-  handleDaysClick(day) {
-    let days = [...this.state.repeatAt];
-    if (this.state.repeatAt.includes(day)) {
-      const removedDays = days.filter(incomingDay => incomingDay !== day);
-      days = removedDays;
-    } else {
-      days = [...this.state.repeatAt, day];
-    }
-    this.setState({ repeatAt: days });
-  }
+		this.setState({ repeatType, readyToSend: true });
+	}
 
-  onSubmit(e) {
-    e.preventDefault();
-    const email = {
-      recipient: this.state.recipient,
-      text: this.state.text,
-      date: this.state.date,
-      sendType: this.state.sendType,
-      repeatType: this.state.repeatType,
-      repeatAt: this.state.repeatAt,
-      neverEnd: this.state.neverEnd,
-      endOn: this.state.endOn,
-      endAfterOccurrences: this.state.endAfterOccurrences,
-    };
+	handleDaysClick(day) {
+		let days = [ ...this.state.repeatAt ];
+		if (this.state.repeatAt.includes(day)) {
+			const removedDays = days.filter((incomingDay) => incomingDay !== day);
+			days = removedDays;
+		} else {
+			days = [ ...this.state.repeatAt, day ];
+		}
+		this.setState({ repeatAt: days });
+	}
 
-    sendMail(email);
-    this.setState(this.baseState);
-  }
+	onSubmit(e) {
+		e.preventDefault();
+		const email = {
+			recipient: this.state.recipient,
+			text: this.state.text,
+			date: this.state.date,
+			sendType: this.state.sendType,
+			repeatType: this.state.repeatType,
+			repeatAt: this.state.repeatAt,
+			neverEnd: this.state.neverEnd,
+			endOn: this.state.endOn,
+			endAfterOccurrences: this.state.endAfterOccurrences
+		};
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <form onSubmit={this.onSubmit}>
-          <EmailInput onChange={this.onChange} name="recipient" value={this.state.recipient} />
-          <TextInput onChange={this.onChange} name="text" value={this.state.text} />
-          <SendOptions onChange={this.onChange} name="now" now={this.state.sendType} />
-          {this.state.sendType === 'custom' && <RepeatType onChange={this.handleTypeChange} />}
-          {this.state.repeatType === 'weekly'
-					&& this.state.sendType === 'custom' && <Days onChange={this.handleDaysClick} />}
-          {(this.state.sendType === 'later' || this.state.sendType === 'custom') && (
-          <DatePicker onChange={this.handleDateChange} />
-          )}
-          {this.state.sendType === 'custom' && (
-          <Ends
-            onChange={this.onChange}
-            handleOccurrencesUp={this.handleOccurrencesUp}
-            handleOccurrencesDown={this.handleOccurrencesDown}
-            onDateChange={this.handleDateChange}
-            handleNeverEnd={this.handleNeverEnd}
-          />
-          )}
-          <SendButton handleClick={this.onSubmit} active={this.state.readyToSend} />
-        </form>
-      </React.Fragment>
-    );
-  }
+		sendMail(email);
+		this.setState(this.baseState);
+		this.setState({ sent: true });
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Header />
+				<br />
+				{this.state.sent && (
+					<Typography variant="body1" color="primary">
+						Email sent...
+					</Typography>
+				)}
+				<form onSubmit={this.onSubmit}>
+					<EmailInput onChange={this.onChange} name="recipient" value={this.state.recipient} />
+					<TextInput onChange={this.onChange} name="text" value={this.state.text} />
+					<SendOptions onChange={this.onChange} name="now" now={this.state.sendType} />
+					{this.state.sendType === 'custom' && <RepeatType onChange={this.handleTypeChange} />}
+					{this.state.repeatType === 'weekly' &&
+					this.state.sendType === 'custom' && <Days onChange={this.handleDaysClick} />}
+					{(this.state.sendType === 'later' || this.state.sendType === 'custom') && (
+						<DatePicker onChange={this.handleDateChange} />
+					)}
+					{this.state.sendType === 'custom' && (
+						<Ends
+							onChange={this.onChange}
+							handleOccurrencesUp={this.handleOccurrencesUp}
+							handleOccurrencesDown={this.handleOccurrencesDown}
+							onDateChange={this.handleDateChange}
+							handleNeverEnd={this.handleNeverEnd}
+						/>
+					)}
+					<SendButton handleClick={this.onSubmit} active={this.state.readyToSend} />
+				</form>
+			</React.Fragment>
+		);
+	}
 }
 
 export default LandingPage;
